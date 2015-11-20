7db05f7cbc15b0ebfd6e8644e7eb8cbf88e4d681
llluiop <290522165@163.com>
2015-11-20 10:40:57 +0800

    add excel sheet

---
+++ Excel Sheet Column Number/main.cpp
@@ -0,0 +1,14 @@
+#include "sheet.h"
+#include <iostream>
+
+
+int main()
+{
+	string s = "AB";
+
+
+	Solution sl;
+	cout << sl.titleToNumber(s) << endl;
+
+	return getchar();
+}
---
+++ Excel Sheet Column Number/sheet.h
@@ -0,0 +1,23 @@
+#ifndef __SHEET_H__
+#define __SHEET_H__
+
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+	int titleToNumber(string s) {
+		int num = 0;
+		
+		int len = s.length();
+		for (int i = 0; i < len; i++)
+		{
+			num *= 26;
+			num += (s[i] - 64);
+		}
+
+		return num;
+	}
+};
+
+#endif
