11e46727235f223c330640c4c22431ce6ba1f37e
llluiop <290522165@163.com>
2015-12-12 14:25:55 +0800

    add remove nth node

---
+++ Word Pattern/main.cpp
@@ -0,0 +1,54 @@
+#include <string>
+#include <map>
+using namespace std;
+#include <sstream>
+
+class Solution {
+public:
+	bool wordPattern(string pattern, string str) {
+		istringstream istr(str);
+
+
+		string word = "";
+		int i = 0;
+		for (; i < pattern.size(); i++)
+		{
+			if (!(istr >> word))
+				return false;
+
+			if (m_.find(pattern[i]) != m_.end())
+			{
+
+				if (word != m_[pattern[i]])
+				{
+					return false;
+				}
+			}
+			else
+			{
+				for (auto iter : m_)
+				{
+					if (m_[iter.first] == word)
+					{
+						return false;
+					}
+				}
+				m_[pattern[i]] = word;
+			}
+		}
+
+		return i == pattern.size() && !(istr>>word);
+	}
+
+private:
+	map<char, string> m_;
+};
+
+
+
+int main()
+{
+	Solution s;
+	s.wordPattern("abba", "dog cat cat dog");
+	return getchar();
+}
