64139935c9a9ee16d088beffa1c5ec3e1c65fc11
Yu Liu <290522165@163.com>
2016-04-05 20:24:07 +0800

    done

---
+++ Two Sum/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+
+class Solution {
+public:
+	std::vector<int> twoSum(std::vector<int>& nums, int target) {
+		std::vector<int> indicies(nums.size());
+		std::iota(std::begin(indicies), std::end(indicies), 0);
+		std::sort(indicies.begin(), indicies.end(), [&nums](int a, int b) {
+			return nums[a] < nums[b];
+		});
+		auto lo = std::begin(indicies);
+		auto hi = std::end(indicies) - 1;
+		while (lo < hi) {
+			if (nums[*lo] + nums[*hi] < target) {
+				lo++;
+			}
+			else if (nums[*lo] + nums[*hi] > target) {
+				hi--;
+			}
+			else /* (nums[*lo] + nums[*hi] == target) */ {
+				break;
+			}
+		}
+		return std::vector<int> { *lo, *hi };
+	}
+};
+
+
+int main()
+{
+	Solution s;
+	auto v = s.twoSum(vector<int>{2, 4, 11, 7}, 9);
+	return getchar();
+}
