3a3171db285a1f514c38bb7dc54c00b8f08e2add
Yu Liu <290522165@163.com>
2016-04-25 19:42:58 +0800

    reivew

---
+++ Find Peak Element/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	int findPeakElement(vector<int>& nums) {
+		int left = 0;
+		int right = nums.size() - 1;
+
+		while (left < right)
+		{
+			int mid = (left + right) / 2;
+			if (nums[mid] > nums[mid+1])
+			{
+				right = mid;
+			}
+			else
+			{
+				left = mid+1;
+			}
+		}
+
+		return left;
+	}
+};
+
+
+int main()
+{
+	Solution s;
+
+	vector<int> v = { -3, -2, -2, -3 };
+	std::cout << s.findPeakElement(v);
+
+	return getchar();
+}
